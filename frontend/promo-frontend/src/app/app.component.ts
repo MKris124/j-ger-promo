d1a610e2a7931eed9d34e22f34bc9b648bce26f4
MKris124 <kristofmora124@gmail.com>
2026-03-17 21:34:17 +0100

    reload

--- frontend/promo-frontend/src/app/app.component.ts
+++ frontend/promo-frontend/src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as PullToRefresh from 'pulltorefreshjs';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'promo-frontend';
+export class AppComponent implements OnInit, OnDestroy {
+  
+  ngOnInit() {
+    // 2. Inicializáljuk a "lehúzós" frissítőt
+    PullToRefresh.init({
+      mainElement: 'body', // Melyik elem húzható le
+      instructionsPullToRefresh: '',
+      instructionsReleaseToRefresh: '',
+      instructionsRefreshing: '',
+      // Ha lefutott a húzás, egyszerűen újratöltjük a böngészőablakot
+      onRefresh() {
+        window.location.reload();
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    // Memóriaszivárgás megelőzése
+    PullToRefresh.destroyAll();
+  }
 }
