0f81b7936734fc891fa2e7d20e53e5d62452f136
MKris124 <kristofmora124@gmail.com>
2026-04-24 17:38:44 +0200

    cross origin deletion

--- promo-backend/src/main/java/hu/jager/promo_backend/controller/AuthController.java
+++ promo-backend/src/main/java/hu/jager/promo_backend/controller/AuthController.java
@@ -17,7 +17,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
     private final AuthService authService;
