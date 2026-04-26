88c84f9d595c09ec903869692703591bd6f1ea6e
MKris124 <kristofmora124@gmail.com>
2026-04-26 13:55:46 +0200

    style changes

--- frontend/promo-frontend/tailwind.config.js
+++ frontend/promo-frontend/tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}", // Ebből tudja a Tailwind, hogy a mi fájljainkat kell figyelnie
+    "./src/**/*.{html,ts}",
   ],
   theme: {
     extend: {
       colors: {
-        'jager-orange': '#F37021', // A klasszikus narancssárga szín
-        'jager-dark': '#0f0f0f',   // Sötétebb lett, még jobb a kontraszt
-        'jager-light': '#F5F5F5',  // Törtfehér szövegekhez
-        'jager-green': '#14251a',  // Új: Klasszikus sötét üvegzöld a Jäger palackról
-        'jager-green-light': '#1e3827' // Új: Világosabb zöld a hover effektekhez
+        'jager-orange': '#F37021', // Megtartjuk a gomboknak/fő akcentusnak
+        'jager-amber': '#C66C23',  // ÚJ: Az ital elegáns borostyán színe (finomabb ragyogás)
+        
+        'jager-dark': '#060B08',   // Majdnem fekete, de valójában nagyon sötét zöld
+        'jager-green': '#112217',  // A klasszikus sötét üvegzöld
+        'jager-green-light': '#1A3323', // Világosabb zöld a kártyákhoz/keretekhez
+        
+        'jager-light': '#E8EDE9'   // Törtfehér, leheletnyi zöldes-szürkés hűvösséggel
       }
     },
   },
