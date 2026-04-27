5a4c959f9f37fa385567a6b8b2a1e36cc77d647a
MKris124 <kristofmora124@gmail.com>
2026-04-27 18:57:33 +0200

    Silent role update

--- promo-backend/src/main/java/hu/jager/promo_backend/security/JwtAuthFilter.java
+++ promo-backend/src/main/java/hu/jager/promo_backend/security/JwtAuthFilter.java
@@ -68,6 +68,10 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
         String path = request.getRequestURI();
+
+        if (path.equals("/api/auth/refresh")) {
+            return false;
+        }
         // A filter NEM fut le a H2 konzolon és az auth végpontokon
         return path.startsWith("/h2-console") || path.startsWith("/api/auth/");
     }
